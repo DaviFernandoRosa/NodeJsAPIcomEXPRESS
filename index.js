917c71aaef7e3066e38e71603a9b14442ffc13c0
Davi Fernando Rosa <davir0514@gmail.com>
2021-07-10 16:05:07 -0300

    Arquivos

---
+++ index.js
@@ -0,0 +1,20 @@
+//chamando as dependencias que instalamos
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const routes = require('./config/routes')
+
+
+const app = express()
+//adicionando as bibiliotecas dentro do app
+app.use(morgan('dev'))
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(express.json())
+app.use(cors())
+app.use(routes) 
+
+// Ligando o servidor
+app.listen(21262, ()=>{
+    console.log(`Express started at http://localhost:21262`)
+})
